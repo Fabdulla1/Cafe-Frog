ec0095613f8ea7a8e6162bb76d3f076db2af1e7d
farhan <farhan_abdulla@berkeley.edu>
2024-01-17 12:25:29 -0800

    git test

---
+++ CafeFrog/Cafe Frog.py
@@ -0,0 +1,20 @@
+import pygame as py
+
+class GameEngine:
+    def __init__(self):
+        py.init()
+        Screens().mainMenu
+
+        
+    
+    
+class Screens:
+    def mainMenu():
+        screen = py.display.set_mode(500, 500)
+        py.draw.rect(screen, "red", 3, 4)
+
+
+GameEngine.__init__
+
+        
+    
